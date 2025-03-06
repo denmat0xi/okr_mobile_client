46823f131a2ed155f1315e74cebfaa09d6409ec6
Leonid Kondratev <feckfeckfeck@mail.ru>
2025-03-06 23:34:47 +0700

    Defined endpoints according to swagger

--- app/src/main/java/com/denmatoxi/okr_mobile/ApiService.kt
+++ app/src/main/java/com/denmatoxi/okr_mobile/ApiService.kt
@@ -1,40 +1,84 @@
 package com.denmatoxi.okr_mobile
 
-import com.denmatoxi.okr_mobile.dataClasses.AuthResponse
-import com.denmatoxi.okr_mobile.dataClasses.FileUploadResponse
-import com.denmatoxi.okr_mobile.dataClasses.LoginRequest
-import com.denmatoxi.okr_mobile.dataClasses.Pass
-import com.denmatoxi.okr_mobile.dataClasses.PassListResponse
-import com.denmatoxi.okr_mobile.dataClasses.PassRequest
-import com.denmatoxi.okr_mobile.dataClasses.PassResponse
+import com.denmatoxi.okr_mobile.DataClasses.AuthResponse
+import com.denmatoxi.okr_mobile.DataClasses.FileUploadResponse
+import com.denmatoxi.okr_mobile.DataClasses.LoginRequest
+import com.denmatoxi.okr_mobile.DataClasses.Pass
+import com.denmatoxi.okr_mobile.DataClasses.PassListResponse
+import com.denmatoxi.okr_mobile.DataClasses.PassRequest
+import com.denmatoxi.okr_mobile.DataClasses.PassResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiService {
+    //Account
+    @POST("/registration")
+    fun register()
 
-    @POST("auth/login")
-    fun login(@Body request: LoginRequest): Call<AuthResponse>
+    @POST("/login")
+    fun getPass()
 
-    @GET("passes/{id}")
-    fun getPass(@Path("id") passId: Int): Call<Pass>
+    @POST("/logout")
+    fun uploadFile()
 
-    @Multipart
-    @POST("passes/upload")
-    fun uploadFile(
-        @Part file: MultipartBody.Part,
-        @Part("description") description: RequestBody
-    ): Call<FileUploadResponse>
+    @GET("/profile")
+    fun createPass()
 
-    @POST("passes/create")
-    fun createPass(@Body request: PassRequest): Call<PassResponse>
+    @PUT("/profile")
+    fun getPasses()
 
-    @GET("passes")
-    fun getPasses(): Call<PassListResponse>
+    //Application
+    @GET("/application")
+    fun name1()
+
+    @POST("/application")
+    fun name2()
+
+    @PUT("/application/{id}")
+    fun name3()
+
+    @DELETE("/application/{id}")
+    fun name4()
+
+    @GET("/application/my")
+    fun name5()
+
+    @POST("/application/{id}/status")
+    fun name6()
+
+    //ExtensionApplication
+    @POST("/extensionApplication/{applicationId}")
+    fun ext1()
+
+    @PUT("/extensionApplication/{id}")
+    fun ext2()
+
+    @DELETE("/extensionApplication/{id}")
+    fun ext3()
+
+    @POST("/extensionApplication/{id}/status")
+    fun ext4()
+
+
+    //Users
+    @GET("/roles")
+    fun usr1()
+
+    @POST("/user/{id}/role")
+    fun usr2()
+
+    @GET("/users")
+    fun usr3()
+
+
+
 }
